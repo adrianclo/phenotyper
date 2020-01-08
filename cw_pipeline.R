f6a8b7d450a9461354f66b955cc79ed225047218
adrianclo <adrianclo1984@gmail.com>
2020-01-08 14:38:47 +0100

    update pipeline

--- cw_pipeline.R
+++ cw_pipeline.R
@@ -3,13 +3,14 @@ source("cw_functions.R")
 # FILL IN DATA DIRECTORY # 
 data_dir <- "ADD/HERE/YOUR/DIRECTORY"
 
-# ml <- readRDS(paste0(data_dir, ".RDS"))
+# ml <- readRDS(paste0(data_dir, ".RDS")) # if ml already saved in .RDS format
 ml <- import_raw_cw(data_dir = data_dir, zip = F, trim = 90)
 
-surv <- survival_plot(ml = ml, version = 1, factor_levels = c("WT","KO"))
-surv$data %>% as.data.frame()
+survival_data(ml = ml)
+survival_summary(ml = ml, exclude = NULL, factor_levels = c("WT","KO"))
 
-entry_subtypes(ml = ml, exclude = NULL, factor_levels = c("WT","KO"))
+entries_data(ml = ml)
+entries_summary(ml = ml, exclude = NULL, factor_levels = c("WT","KO"))
 
 time_plot(ml = ml, factor_levels = c("WT","KO"))
 
